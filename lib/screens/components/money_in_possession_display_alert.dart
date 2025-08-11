4f96039cd3176f6b0bf4ef35b49cdf117ceb2f9b
toyodahideyuki <toyodahideyuki4819@gmail.com>
2025-08-11 14:50:46 +0900

    commit 20250811

--- lib/screens/components/money_in_possession_display_alert.dart
+++ lib/screens/components/money_in_possession_display_alert.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:scroll_to_index/scroll_to_index.dart';
 
 import '../../controllers/controllers_mixin.dart';
 import '../../extensions/extensions.dart';
@@ -16,6 +17,9 @@ class MoneyInPossessionDisplayAlert extends ConsumerStatefulWidget {
 
 class _MoneyInPossessionDisplayAlertState extends ConsumerState<MoneyInPossessionDisplayAlert>
     with ControllersMixin<MoneyInPossessionDisplayAlert> {
+  final AutoScrollController autoScrollController = AutoScrollController();
+
+  ///
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,6 +46,30 @@ class _MoneyInPossessionDisplayAlertState extends ConsumerState<MoneyInPossessio
 
                 Divider(color: Colors.white.withOpacity(0.4), thickness: 5),
 
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    const SizedBox.shrink(),
+                    Row(
+                      children: <Widget>[
+                        IconButton(
+                          onPressed: () {
+                            autoScrollController.scrollToIndex(appParamState.keepMoneyMap.length);
+                          },
+                          icon: const Icon(Icons.arrow_downward),
+                        ),
+
+                        IconButton(
+                          onPressed: () {
+                            autoScrollController.scrollToIndex(0);
+                          },
+                          icon: const Icon(Icons.arrow_upward),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+
                 Expanded(child: _displayPossessionMoneyList()),
               ],
             ),
@@ -55,24 +83,52 @@ class _MoneyInPossessionDisplayAlertState extends ConsumerState<MoneyInPossessio
   Widget _displayPossessionMoneyList() {
     final List<Widget> list = <Widget>[];
 
+    int lastSum = 0;
+    int i = 0;
     appParamState.keepMoneyMap.forEach((String key, MoneyModel value) {
       if (key.split('-')[0].toInt() >= 2023) {
         list.add(
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 5),
-            decoration: BoxDecoration(
-              border: Border(bottom: BorderSide(color: Colors.white.withValues(alpha: 0.2))),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[Text(key), Text(value.sum.toCurrency())],
+          AutoScrollTag(
+            // ignore: always_specify_types
+            key: ValueKey(i),
+            index: i,
+            controller: autoScrollController,
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.white.withValues(alpha: 0.2))),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(key),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      Text(value.sum.toCurrency()),
+
+                      if (i == 0)
+                        const SizedBox.shrink()
+                      else
+                        Text(
+                          (lastSum - value.sum.toInt()).toString().toCurrency(),
+                          style: const TextStyle(color: Colors.grey, fontSize: 10),
+                        ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         );
+
+        lastSum = value.sum.toInt();
+        i++;
       }
     });
 
     return CustomScrollView(
+      controller: autoScrollController,
       slivers: <Widget>[
         SliverList(
           delegate: SliverChildBuilderDelegate(
