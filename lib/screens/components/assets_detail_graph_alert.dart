a03c71d38acd315b4584719783c0e5ff0dbdc8aa
toyodahideyuki <toyodahideyuki4819@gmail.com>
2025-08-18 18:43:12 +0900

    commit 20250818

--- lib/screens/components/assets_detail_graph_alert.dart
+++ lib/screens/components/assets_detail_graph_alert.dart
@@ -54,26 +54,28 @@ class _AssetsDetailGraphAlertState extends ConsumerState<AssetsDetailGraphAlert>
 
                 Divider(color: Colors.white.withOpacity(0.4), thickness: 5),
 
-                Expanded(child: displayAssetsNameList()),
+                if (widget.title != 'gold') ...<Widget>[
+                  Expanded(child: displayAssetsNameList()),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    const SizedBox.shrink(),
-                    GestureDetector(
-                      onTap: () {
-                        appParamNotifier.setSelectedGraphInvestNameModel();
-                      },
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      const SizedBox.shrink(),
+                      GestureDetector(
+                        onTap: () {
+                          appParamNotifier.setSelectedGraphInvestNameModel();
+                        },
 
-                      child: const Icon(Icons.close),
-                    ),
-                  ],
-                ),
+                        child: const Icon(Icons.close),
+                      ),
+                    ],
+                  ),
 
-                Divider(color: Colors.white.withOpacity(0.4), thickness: 5),
+                  Divider(color: Colors.white.withOpacity(0.4), thickness: 5),
+                ],
 
                 SizedBox(
-                  height: (widget.title == 'stock') ? 500 : 400,
+                  height: (widget.title == 'gold') ? context.screenSize.height * 0.7 : context.screenSize.height * 0.5,
                   child: Stack(children: <Widget>[LineChart(graphData2), LineChart(graphData)]),
                 ),
               ],
@@ -88,31 +90,37 @@ class _AssetsDetailGraphAlertState extends ConsumerState<AssetsDetailGraphAlert>
   void setChartData() {
     flspotsList.clear();
 
-    String roopTitle = widget.title;
-    if (widget.title == 'toushiShintaku') {
-      roopTitle = 'shintaku';
-    }
-
     List<String> dateList = <String>[];
 
-    appParamState.keepInvestNamesMap[roopTitle]
-      ?..sort((InvestNameModel a, InvestNameModel b) => a.frame.compareTo(b.frame))
-      ..sort((InvestNameModel a, InvestNameModel b) => a.relationalId.compareTo(b.relationalId))
-      ..forEach((InvestNameModel element) {
-        bool flag = true;
-
-        if (appParamState.selectedGraphInvestNameModel != null) {
-          if (appParamState.selectedGraphInvestNameModel != element) {
-            flag = false;
-          }
-        }
-
-        if (flag) {
-          appParamState.keepInvestRecordMap[element.relationalId]?.forEach((InvestRecordModel element2) {
-            dateList.add(element2.date);
-          });
-        }
+    if (widget.title == 'gold') {
+      appParamState.keepInvestRecordMap[0]?.forEach((InvestRecordModel element2) {
+        dateList.add(element2.date);
       });
+    } else {
+      String roopTitle = widget.title;
+      if (widget.title == 'toushiShintaku') {
+        roopTitle = 'shintaku';
+      }
+
+      appParamState.keepInvestNamesMap[roopTitle]
+        ?..sort((InvestNameModel a, InvestNameModel b) => a.frame.compareTo(b.frame))
+        ..sort((InvestNameModel a, InvestNameModel b) => a.relationalId.compareTo(b.relationalId))
+        ..forEach((InvestNameModel element) {
+          bool flag = true;
+
+          if (appParamState.selectedGraphInvestNameModel != null) {
+            if (appParamState.selectedGraphInvestNameModel != element) {
+              flag = false;
+            }
+          }
+
+          if (flag) {
+            appParamState.keepInvestRecordMap[element.relationalId]?.forEach((InvestRecordModel element2) {
+              dateList.add(element2.date);
+            });
+          }
+        });
+    }
 
     dateList = dateList.toSet().toList();
     dateList.sort();
@@ -132,33 +140,53 @@ class _AssetsDetailGraphAlertState extends ConsumerState<AssetsDetailGraphAlert>
 
     final Map<int, List<int>> dateMaxValueMapData = <int, List<int>>{};
 
-    appParamState.keepInvestNamesMap[roopTitle]
-      ?..sort((InvestNameModel a, InvestNameModel b) => a.frame.compareTo(b.frame))
-      ..sort((InvestNameModel a, InvestNameModel b) => a.dealNumber.compareTo(b.dealNumber))
-      ..forEach((InvestNameModel element) {
-        bool flag = true;
+    if (widget.title == 'gold') {
+      final List<FlSpot> flspots = <FlSpot>[];
+      appParamState.keepInvestRecordMap[0]?.forEach((InvestRecordModel element2) {
+        final int pos = dateList.indexWhere((String element) => element == element2.date);
 
-        if (appParamState.selectedGraphInvestNameModel != null) {
-          if (appParamState.selectedGraphInvestNameModel != element) {
-            flag = false;
-          }
-        }
+        flspots.add(FlSpot(pos.toDouble(), (element2.price - element2.cost).toDouble()));
 
-        if (flag) {
-          final List<FlSpot> flspots = <FlSpot>[];
-          appParamState.keepInvestRecordMap[element.relationalId]?.forEach((InvestRecordModel element2) {
-            final int pos = dateList.indexWhere((String element) => element == element2.date);
+        list.add(element2.price - element2.cost);
 
-            flspots.add(FlSpot(pos.toDouble(), (element2.price - element2.cost).toDouble()));
-
-            list.add(element2.price - element2.cost);
-
-            (dateMaxValueMapData[pos] ??= <int>[]).add(element2.price - element2.cost);
-          });
-
-          flspotsList.add(flspots);
-        }
+        (dateMaxValueMapData[pos] ??= <int>[]).add(element2.price - element2.cost);
       });
+
+      flspotsList.add(flspots);
+    } else {
+      String roopTitle = widget.title;
+      if (widget.title == 'toushiShintaku') {
+        roopTitle = 'shintaku';
+      }
+
+      appParamState.keepInvestNamesMap[roopTitle]
+        ?..sort((InvestNameModel a, InvestNameModel b) => a.frame.compareTo(b.frame))
+        ..sort((InvestNameModel a, InvestNameModel b) => a.dealNumber.compareTo(b.dealNumber))
+        ..forEach((InvestNameModel element) {
+          bool flag = true;
+
+          if (appParamState.selectedGraphInvestNameModel != null) {
+            if (appParamState.selectedGraphInvestNameModel != element) {
+              flag = false;
+            }
+          }
+
+          if (flag) {
+            final List<FlSpot> flspots = <FlSpot>[];
+            appParamState.keepInvestRecordMap[element.relationalId]?.forEach((InvestRecordModel element2) {
+              final int pos = dateList.indexWhere((String element) => element == element2.date);
+
+              flspots.add(FlSpot(pos.toDouble(), (element2.price - element2.cost).toDouble()));
+
+              list.add(element2.price - element2.cost);
+
+              (dateMaxValueMapData[pos] ??= <int>[]).add(element2.price - element2.cost);
+            });
+
+            flspotsList.add(flspots);
+          }
+        });
+    }
 
     if (list.isNotEmpty) {
       final int warisuu = (widget.title == 'stock') ? 10000 : 50000;
@@ -193,12 +221,6 @@ class _AssetsDetailGraphAlertState extends ConsumerState<AssetsDetailGraphAlert>
               final List<String> toolTipDisplayValue = <String>[];
 
               for (final LineBarSpot element in touchedSpots) {
-                final TextStyle textStyle = TextStyle(
-                  color: element.bar.gradient?.colors.first ?? element.bar.color ?? Colors.blueGrey,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 12,
-                );
-
                 final String date = dateList[element.x.toInt()];
 
                 final int? maxPrice = dateMaxValueMap[element.x.toInt()];
@@ -210,7 +232,17 @@ class _AssetsDetailGraphAlertState extends ConsumerState<AssetsDetailGraphAlert>
 
                 toolTipDisplayValue.add(price);
 
-                list.add(LineTooltipItem(toolTipDisplayValue.join('\n'), textStyle, textAlign: TextAlign.end));
+                list.add(
+                  LineTooltipItem(
+                    toolTipDisplayValue.join('\n'),
+                    TextStyle(
+                      color: (element.y < 0) ? const Color(0xFFFBB6CE) : Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12,
+                    ),
+                    textAlign: TextAlign.end,
+                  ),
+                );
 
                 toolTipDisplayValue.clear();
               }
@@ -247,7 +279,7 @@ class _AssetsDetailGraphAlertState extends ConsumerState<AssetsDetailGraphAlert>
               spots: flspotsList[i],
               barWidth: 1,
               isStrokeCapRound: true,
-              color: (appParamState.selectedGraphInvestNameModel != null)
+              color: (appParamState.selectedGraphInvestNameModel != null || widget.title == 'gold')
                   ? Colors.white.withValues(alpha: 0.5)
                   : twelveColor[i % 12],
 
@@ -333,57 +365,84 @@ class _AssetsDetailGraphAlertState extends ConsumerState<AssetsDetailGraphAlert>
   Widget displayAssetsNameList() {
     final List<Widget> list = <Widget>[];
 
-    String roopTitle = widget.title;
-    if (widget.title == 'toushiShintaku') {
-      roopTitle = 'shintaku';
-    }
-
     int i = 0;
 
     final List<Color> twelveColor = utility.getTwelveColor();
 
-    appParamState.keepInvestNamesMap[roopTitle]
-      ?..sort((InvestNameModel a, InvestNameModel b) => a.frame.compareTo(b.frame))
-      ..sort((InvestNameModel a, InvestNameModel b) => a.dealNumber.compareTo(b.dealNumber))
-      ..forEach((InvestNameModel element) {
-        list.add(
-          DefaultTextStyle(
-            style: const TextStyle(fontSize: 12),
+    if (widget.title == 'gold') {
+    } else {
+      String roopTitle = widget.title;
+      if (widget.title == 'toushiShintaku') {
+        roopTitle = 'shintaku';
+      }
 
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border(bottom: BorderSide(color: Colors.white.withOpacity(0.3))),
-              ),
-              padding: const EdgeInsets.all(5),
+      appParamState.keepInvestNamesMap[roopTitle]
+        ?..sort((InvestNameModel a, InvestNameModel b) => a.frame.compareTo(b.frame))
+        ..sort((InvestNameModel a, InvestNameModel b) => a.dealNumber.compareTo(b.dealNumber))
+        ..forEach((InvestNameModel element) {
+          int lastDiff = 0;
 
-              child: Row(
-                children: <Widget>[
-                  GestureDetector(
-                    onTap: () => appParamNotifier.setSelectedGraphInvestNameModel(investNameModel: element),
-                    child: CircleAvatar(
-                      radius: 15,
-                      backgroundColor: (appParamState.selectedGraphInvestNameModel == element)
-                          ? Colors.white.withValues(alpha: 0.5)
-                          : twelveColor[i % 12].withValues(alpha: 0.3),
+          if (appParamState.keepInvestRecordMap[element.relationalId] != null) {
+            final InvestRecordModel last = appParamState.keepInvestRecordMap[element.relationalId]!.last;
+
+            lastDiff = last.price - last.cost;
+          }
+
+          list.add(
+            DefaultTextStyle(
+              style: const TextStyle(fontSize: 12),
+
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border(bottom: BorderSide(color: Colors.white.withOpacity(0.3))),
+                ),
+                padding: const EdgeInsets.all(5),
+
+                child: Row(
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: () => appParamNotifier.setSelectedGraphInvestNameModel(investNameModel: element),
+                      child: CircleAvatar(
+                        radius: 15,
+                        backgroundColor: (appParamState.selectedGraphInvestNameModel == element)
+                            ? Colors.white.withValues(alpha: 0.5)
+                            : twelveColor[i % 12].withValues(alpha: 0.3),
+                      ),
                     ),
-                  ),
 
-                  const SizedBox(width: 20),
+                    const SizedBox(width: 20),
 
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[Text(element.frame), Text(element.name)],
+                    Expanded(
+                      child: ConstrainedBox(
+                        constraints: const BoxConstraints(minHeight: 70),
+
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(element.frame),
+                            Text(element.name),
+
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[const SizedBox.shrink(), Text(lastDiff.toString().toCurrency())],
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                ],
+
+                    const SizedBox(width: 20),
+
+                    Icon(Icons.list, color: Colors.white.withValues(alpha: 0.4)),
+                  ],
+                ),
               ),
             ),
-          ),
-        );
+          );
 
-        i++;
-      });
+          i++;
+        });
+    }
 
     return CustomScrollView(
       slivers: <Widget>[
