4e96babdf32bc61258b15b6a953ed436eaea3c84
toyodahideyuki <toyodahideyuki4819@gmail.com>
2025-11-10 17:44:20 +0900

    commit 20251110

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -6,8 +7,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'controllers/controllers_mixin.dart';
 import 'screens/home_screen.dart';
 
-void main() async {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
+
+  await SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
   runApp(const AppRoot());
 }
